70663bdb4df53d79d17e8e38cadbc4e07b6a61c7
Hyunmok Choi <37583279+hyunnnchoi@users.noreply.github.com>
2024-01-05 18:03:53 +0900

    [level unrated] Title: n개 간격의 원소들, Time: 0.02 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181888. n개 간격의 원소들/n개 간격의 원소들.java
+++ 프로그래머스/unrated/181888. n개 간격의 원소들/n개 간격의 원소들.java
@@ -1,15 +1,18 @@
 import java.util.*;
 
 class Solution {
-    public Integer[] solution(int[] num_list, int n) {
-        List<Integer> a = new ArrayList<>();
+    public int[] solution(int[] num_list, int n) {
+        // int[] 배열 안에.. 
+        int arrSize = num_list.length%n== 0 ? num_list.length/n : num_list.length/n +1;
+        int[] answer = new int[arrSize];
+        // n 단위로 인덱스 접근..
         int count = 0;
-        for(int i=0; i<num_list.length; i++)
-            if(i%n == 0){
-                a.add(num_list[i]);
-            }
-        int aSize = a.size();
-        Integer arr[] = a.toArray(new Integer[aSize]);
-        return arr;
+        // 예를들어, 전체 6개에 n이 4일 경우?
+        // arrSize가 1이되네...
+        for(int i=0; i<num_list.length; i += n){
+            answer[count] = num_list[i];
+            count += 1;
+        }
+        return answer;
     }
 }

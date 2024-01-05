665c5220894d3d13f32458a895ed90cea9ee4634
Hyunmok Choi <37583279+hyunnnchoi@users.noreply.github.com>
2024-01-05 17:38:35 +0900

    [level unrated] Title: n 번째 원소부터, Time: 0.02 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/unrated/181892. n 번째 원소부터/n 번째 원소부터.java
+++ 프로그래머스/unrated/181892. n 번째 원소부터/n 번째 원소부터.java
@@ -1,24 +1,7 @@
-import java.util.Collections;
-import java.util.ArrayList;
-import java.util.List;
-
+import java.util.*;
 class Solution {
     public int[] solution(int[] num_list, int n) {
-        // 새로운 ArrayList 생성
-        List<Integer> arrayList = new ArrayList<>();
-        for(int i:num_list){
-            arrayList.add(i);
-        }
-        // 앞에 n개 제거
-        for(int i=1; i<n; i++){
-            arrayList.remove(0);
-        }
-        // arrayList -> list
-        
-        int[] array = new int[arrayList.size()];
-        for(int i=0; i<arrayList.size(); i++){
-            array[i] = arrayList.get(i);
-        }
-        return array;
+        int[] a = Arrays.copyOfRange(num_list, n-1, num_list.length);
+        return a;
     }
 }
